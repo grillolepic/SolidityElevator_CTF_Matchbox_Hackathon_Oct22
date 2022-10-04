102fe706488fc5d900f9522fc96f9168c983d1c1
0xGrillo <0xGrillo@waveless.finance>
2022-10-04 11:32:52 -0300

    Simple deploy script

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,7 +1,18 @@
 const hre = require("hardhat");
 
 async function main() {
+  const drngFactory = await ethers.getContractFactory("DRNG");
+  const DRNG = await drngFactory.deploy();
 
+  console.log(`DRNG deployed to ${DRNG.address}`);
+
+  const solidityElevatorCtfFactory = await ethers.getContractFactory("SolidityElevatorCTF", {
+    libraries: {
+      DRNG: DRNG.address
+    }
+  });
+  const SECTF = await solidityElevatorCtfFactory.deploy();
+  console.log(`SECTF deployed to ${SECTF.address}`);
 }
 
 main().catch((error) => {
